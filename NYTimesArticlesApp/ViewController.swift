aa4ff7f35fb2f66e41e75cfa6d83714d873b611c
Umar Muzaffar Goraya <umarmuzaffargoraya@uzmans-macbook-pro.local>
2019-05-13 18:00:41 +0500

    Initial Commit

---
+++ NYTimesArticlesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTimesArticlesApp
+//
+//  Created by Umar Muzaffar Goraya on 5/13/19.
+//  Copyright © 2019 Umar Muzaffar Goraya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
